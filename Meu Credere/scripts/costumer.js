4e8ee77d47f1b44fc023e1ecb2aed7a09ae157db
Henrique Silva <99505665+HenriqueSilv4@users.noreply.github.com>
2022-04-08 01:28:11 -0300

    Add files via upload
    Atualizado estilização por CSS usando classes no form.js

---
+++ Meu Credere/scripts/costumer.js
@@ -0,0 +1,57 @@
+const DOM = {
+
+    costumersContainer: document.querySelector('#datatable tbody'),
+
+    addCostumer(costumer, index) {
+
+        const tr = document.createElement('tr');
+        tr.innerHTML = DOM.innerHTMLTransaction(costumer, index);
+        tr.dataset.index = index;
+        DOM.costumersContainer.appendChild(tr);
+
+    },
+
+    innerHTMLTransaction(costumer, index) {
+
+        const html = `
+        <td>${costumer.name}</td>
+        <td>${costumer.birthday}</td>
+        <td>${costumer.state}</td>
+        <td>${costumer.city}</td>
+        
+        <td>${costumer.driver_license_number}</td>
+        <td>${costumer.driver_license_issued_at}</td>
+
+        <td>${costumer.phone_1}</td>
+        <td>${costumer.phone_2}</td>
+        <td>${costumer.phone_3}</td>
+        <td>${costumer.phone_4}</td>
+
+        <td>${costumer.email_1}</td>
+        <td>${costumer.email_2}</td>
+        <td>${costumer.email_3}</td>
+
+        <td>${costumer.sponsor_name}</td>
+        <td>${costumer.sponsor_phone}</td>`;
+
+        return html;
+    }
+
+}
+
+const Storage = {
+
+    get() {
+
+        return JSON.parse(localStorage.getItem("meuCredere:costumers")) || [];
+
+    }
+
+}
+
+const Costumers = {
+
+    all: Storage.get()
+}
+
+Costumers.all.forEach(DOM.addCostumer);
